17d3ad7a927b5c69f33766137d02ebb968e95db3
Shandc1110 <43404966+shandc1110@users.noreply.github.com>
2021-06-02 21:31:10 +0100

    +2

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import numpy as np
+from sqlalchemy import create_engine
 from typing import List, Dict
+from sqlalchemy.types import Integer, Text, String, DateTime, Float, Boolean
+from urllib.parse import quote_plus as urlquote
+
+password = urlquote("Victoria1008@")
+db_uri = 'mysql+pymysql://root:{}@localhost:3306/CATest'.format(password)
+engine = create_engine(db_uri, echo=True)
+
 
 def h_read_and_sort(hfilename):
 
     """Read and sort Holdings data by securityID and EffectiveDate"""
 
     df = pd.read_csv(hfilename, delimiter=',')
+    
     df_h_sort = df.sort_values(by=['SecurityID','EffectiveDate'], ascending=True)
     return df_h_sort
 
@@ -15,7 +24,8 @@ def t_read_and_sort(tfilename):
 
     """Read and sort Transaction data by dealdate"""
     
-    df = pd.read_csv(tfilename,delimiter=',')
+    df = pd.read_csv(tfilename, header=0, delimiter=',')
+    df.columns = ['Description','SecurityID','ISIN','Cusip','Asset Sub-Class', 'Asset Sub-type', 'Cost Currency', 'DealDate','Settlement/Start Date','Nominal','Price','NetConsideration','Accrued','Buy Currency']
     df_t_sort = df.sort_values(by=['DealDate'], ascending=True)
     return df_t_sort
 
@@ -29,7 +39,107 @@ def remove_comma(df_h_sort, headername):
 
 
 
+def calculate_return(df_h_sort):
 
+    df_pivot = pd.pivot_table(df_h_sort,index=['SecurityID'],columns=['EffectiveDate'],values=['TotalMarketValue','Face'] ,aggfunc=[np.sum],fill_value=0)
+    
+    df_pivot.style.format({'return': "{:.2%}"})
+    df_pivot.columns = ['_'.join((j,k,i)) for i,j,k in df_pivot.columns]
+    df_pivot['ID'] = df_pivot.index
+    df_pivot['return'] = ((df_pivot['TotalMarketValue_28-Jan-21_sum'] / df_pivot['Face_28-Jan-21_sum'])   
+    - (df_pivot['TotalMarketValue_27-Jan-21_sum']/df_pivot['Face_27-Jan-21_sum'])) /(
+        df_pivot['TotalMarketValue_27-Jan-21_sum']/df_pivot['Face_27-Jan-21_sum']).apply(lambda x: "%.4f" % x).astype('float64')
+    
+    
+    df_pivot['movement'] = (df_pivot['Face_28-Jan-21_sum'] - df_pivot['Face_27-Jan-21_sum']).astype('float64').apply(lambda x: "%.4f" % x).astype('float64')
+    
+    return df_pivot
+
+def calculate_movement(df_pivot,df_t_sort):
+
+    transactions_fromholdings = df_pivot[df_pivot['movement'] != 0]
+
+    compare_result = pd.merge(transactions_fromholdings,df_t_sort, how='outer',on='SecurityID')
+    notintransaction = compare_result[(compare_result['Nominal'] - compare_result['movement']) !=0 ] 
+
+    return notintransaction
+
+def import_to_database(table_name, schema, jobs_df):
+    jobs_df.to_sql(
+        table_name,
+        engine,
+        if_exists ='replace',
+        index=False,
+        chunksize=500,
+        dtype=schema
+    )
+
+HOLDINGS_SCHEMA = {
+    'EffectiveDate':        String(50),
+    'SecurityID':           String(50),
+    'Description':          String(200),
+    'AssetClass':           String(50),
+    'SubClass':             String(50),
+    'Currency':             String(50),
+    'Face':                 Float,
+    'Price':                Float,
+    'TotalMarketValue':     Float,
+    'Accrued':              Float,
+    'mvpreacc':             Float,
+    'tmvrc':                Float,
+    'dq1':                  Boolean
+}
+
+TRANSACTION_SCHEMA = {
+    'Description':               String(200),
+    'SecurityID':                String(50),
+    'ISIN':                      String(50),
+    'Cusip':                     String(50),
+    'Asset Sub-Class':           String(50),
+    'Asset Sub-type':            String(50),
+    'Cost Currency':             String(50),
+    'DealDate':                  String(50),
+    'Settlement/Start Date':     String(50),
+    'Nominal':                   Float,
+    'Price':                     Float,
+    'NetConsideration':          Float,
+    'Accrued':                   Float,
+    'Buy Currency':              String(50)
+}
+
+RETURN_CALCULATION_SCHEMA = {
+    'Face_27-Jan-21_sum':                  Float,
+    'Face_28-Jan-21_sum':                  Float,
+    'TotalMarketValue_27-Jan-21_sum':      Float,
+    'TotalMarketValue_28-Jan-21_sum':      Float,
+    'ID':                                  String(50),
+    'return':                              Float,
+    'movement':                            Float
+    }
+
+EXCEPTIONS_SCHEMA = {
+    'SecurityID':                           String(50),
+    'Face_27-Jan-21_sum':                   Float,
+    'Face_28-Jan-21_sum':                   Float,
+    'TotalMarketValue_27-Jan-21_sum':       Float,
+    'TotalMarketValue_28-Jan-21_sum':       Float,
+    'ID':                                   String(50),
+    'return':                               Float,
+    'movement':                             Float,
+    'Description':                          String(200),
+    'ISIN':                                 String(50),
+    'Cusip':                                String(50),
+    'Asset Sub-Class':                      String(50),
+    'Asset Sub-type':                       String(50),
+    'Cost Currency':                        String(50),
+    'DealDate':                             String(50),
+    'Settlement/Start Date':                String(50),
+    'Nominal':                              Float,
+    'Price':                                Float,
+    'NetConsideration':                     Float,
+    'Accrued':                              Float,
+    'Buy Currency':                         String(50)
+    }
 
 
 if __name__ == '__main__':    
@@ -44,29 +154,26 @@ if __name__ == '__main__':
     df_t_sort = t_read_and_sort('Transactions.csv')
  
     
-    """Remove comma for Face, TotalMarketValue and Accrued fields"""
-    headers = ['Face','TotalMarketValue','Accrued']
-    for header in headers:
+    """Remove comma for Face, TotalMarketValue and Accrued fields in Holdings"""
+    h_headers = ['Face','TotalMarketValue','Accrued']
+    for header in h_headers:
         df_h_sort = remove_comma(df_h_sort, header)
+
+    """Remove comma for Nominal, NetConsideration, Accrued in Transaction"""
+
+    t_headers = ['Nominal','NetConsideration','Accrued']
+    for header in t_headers:
+        df_t_sort = remove_comma(df_t_sort, header)
     
     
-    df_pivot = pd.pivot_table(df_h_sort,index=['SecurityID'],columns=['EffectiveDate'],values=['TotalMarketValue','Face'] ,aggfunc=[np.sum],fill_value=0)
-    df_pivot.style.format({'return': "{:.2%}"})
-    df_pivot.columns = ['_'.join((j,k,i)) for i,j,k in df_pivot.columns]
-    
-    df_pivot['return'] = ((df_pivot['TotalMarketValue_28-Jan-21_sum'] - df_pivot['TotalMarketValue_27-Jan-21_sum']) / df_pivot['TotalMarketValue_27-Jan-21_sum'] * 100).apply(lambda x: "%.4f" % x).astype('float64')
-    df_pivot['movement'] = (df_pivot['Face_28-Jan-21_sum'] - df_pivot['Face_27-Jan-21_sum']).astype('float64').apply(lambda x: "%.4f" % x).astype('float64')
-    
-    Transactions = df_pivot[df_pivot['movement'] != 0]
-    
-    print('=========================')
-    print(df_t_sort)
-    print(df_pivot)
-    print(df_pivot.dtypes)
+    """Create a pivot table to calculate returns and transaction movement"""
+
+    df_pivot = calculate_return(df_h_sort)
    
-    print(Transactions)
     
-   
+    """Create transactions_fromholdings table to extract all Face value movement during 2 days"""
+    
+    notintransaction = calculate_movement(df_pivot, df_t_sort)
     
     """Calculate Market value of the instruments of the day"""
     df_h_sort['mvpreacc'] = (df_h_sort['Face'] * df_h_sort['Price'] / 100)
@@ -75,9 +182,28 @@ if __name__ == '__main__':
     df_h_sort['tmvrc'] = df_h_sort['mvpreacc'] + df_h_sort['Accrued']
 
     """Data Quatlity check 1, check Total market value is equal to calculated market value"""
-    df_h_sort['dq1'] = (df_h_sort['tmvrc'] == df_h_sort['TotalMarketValue'] )
+    df_h_sort['dq1'] = (df_h_sort['tmvrc'] - df_h_sort['TotalMarketValue'] < 1 ) 
     
-
+    import_to_database("holdings", HOLDINGS_SCHEMA, df_h_sort)
+    import_to_database("transactions", TRANSACTION_SCHEMA, df_t_sort)
+    import_to_database('return', RETURN_CALCULATION_SCHEMA, df_pivot)
+    import_to_database('exceptions',EXCEPTIONS_SCHEMA, notintransaction)
+    
+    print('=================================================================')
+    print('Print Holdings information with data quality check and recalculation')
     print(df_h_sort)
    
+    print('Print Transaction information with sorting by DealDate')
+    print(df_t_sort)
+    
+    print('Print Holdings information with return calculated')
+    print(df_pivot)
+    
+    print('Print transactions identified from Holdings')
+    print(transactions_fromholdings)
+    
+    print('Compare Transactions identified from Holdings with Transactions table provided to identify differences')
+    print(notintransaction)
    
+   
+    
